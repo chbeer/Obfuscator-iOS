9c266196becc67d3c0312852e936cc00324960df
Christian Beer <christian.beer@chbeer.de>
2024-08-21 07:57:09 +0200

    Swift package

--- Package.swift
+++ Package.swift
@@ -5,12 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Obfuscator",
     products: [.library(name: "Obfuscator", targets: ["Obfuscator"])],
-    targets: [.target(name: "Obfuscator",
-                      path: ".",
-                      exclude: ["Obfuscator.podspec", "./Example/ExampleTests/"],
-                      resources: [.copy("./Example"),
-                                  .copy("README.md"),
-                                  .copy("LICENSE")],
-                      publicHeadersPath: "include"
-    )]
+    targets: [
+        .target(name: "Obfuscator")
+    ]
 )
